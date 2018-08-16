eed147971493218efe4c89f72887e77472671f10
Wagner Silveira <wagner.silveira@theta.co.nz>
2018-08-17 10:47:56 +1200

    Update Logic Apps powershell scripts

---
+++ Remove-AzureResource/Remove-AzureResource.ps1
@@ -0,0 +1,4 @@
+﻿param([string] $subscription, [string] $resourceGroup, [string]$ResourceName)
+
+Get-AzureRmSubscription -SubscriptionName $subscription | Select-AzureRmSubscription
+get-AzureRMResource -ResourceName $resourceName -ResourceGroupName $resourceGroup |ForEach-Object {Remove-AzureRMResource -ResourceId $_.ResourceId}
---
+++ Set-ExceptionNotificationAlert/set-RG-ErrorNotificationAlert.ps1
@@ -0,0 +1,11 @@
+﻿param([string] $subscriptioname, [string] $resourcegroup, [string]$emaillist)
+
+Get-AzureRmSubscription -SubscriptionName $subscriptioname | Select-AzureRmSubscription
+
+Find-AzureRmResource -ResourceGroupNameContains $resourcegroup -ResourceType Microsoft.Logic/workflows | ForEach-Object {
+
+    $parameterList = @{logicappname=$_.ResourceName;location=$_.Location;resourceid=$_.ResourceId;emaillist=$emaillist}
+
+    New-AzureRmResourceGroupDeployment -ResourceGroupName $_.ResourceGroupName -TemplateFile LogicAppsExceptionAlertCreation.json -TemplateParameterObject $parameterList
+}
+
